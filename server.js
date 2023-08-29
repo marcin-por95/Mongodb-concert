d8fe3b1d80994091b73e5677c497badb1b143c7e
Marcin Poręba <marcin.poreba1995@gmail.com>
2023-08-29 23:21:01 +0200

    prepare website for replit site

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const db = require('./db');
 
 const app = express();
 
-// import routes
 const testimonialsRouter = require('./routes/testimonials.routes');
 const concertsRouter = require('./routes/concerts.routes');
 const seatsRouter = require('./routes/seats.routes');
@@ -13,18 +13,21 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-/* ENDPOINTS  */
-
 app.use('/api', testimonialsRouter);
 app.use('/api', concertsRouter);
 app.use('/api', seatsRouter);
 
-/* ------------------------------------------------------------ */
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+
 app.use((req, res) => {
     res.status(404).json({ message: 'Not found...' });
 });
 
-// Listening to server on port 8000
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
     console.log('Server is running on port: 8000');
 });
