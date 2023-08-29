04aacbb9168856606e38e4555a96fe1930fdbefc
Marcin Poręba <marcin.poreba1995@gmail.com>
2023-08-29 22:33:56 +0200

    Add the server with a check to determine if the slot is already taken.

---
+++ client/src/redux/store.js
@@ -0,0 +1,21 @@
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+import concerts from './concertsRedux';
+import seats from './seatsRedux';
+
+// combine reducers
+const rootReducer = combineReducers({
+	concerts,
+	seats,
+});
+
+const store = createStore(
+	rootReducer,
+	compose(
+		applyMiddleware(thunk),
+		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	)
+);
+
+export default store;
