0cbd9eb6cf69dfe4f87ff03b1032f0a59a3365ad
Marcin Poręba <marcin.poreba1995@gmail.com>
2023-09-07 13:51:20 +0200

    ADD Free seats functionality

--- client/src/components/features/SeatChooser/SeatChooser.js
+++ client/src/components/features/SeatChooser/SeatChooser.js
@@ -1,38 +1,42 @@
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Button, Progress, Alert } from 'reactstrap';
-import { getSeats, loadSeats, getRequests, loadSeatsRequest } from '../../../redux/seatsRedux';
+import { getSeats, loadSeats, getRequests } from '../../../redux/seatsRedux';
 import './SeatChooser.scss';
 import io from 'socket.io-client';
 
 const getSocketConnectionUrl = () => {
     if (process.env.NODE_ENV === 'production') {
-        // Use default address for production environment
+
         return window.location.origin;
     } else {
-        // Use localhost:8000 for development environment
+
         return 'http://localhost:8000';
     }
 };
 
 const SeatChooser = ({ chosenDay, chosenSeat, updateSeat }) => {
-    const [socket, setSocket] = useState(null);
     const dispatch = useDispatch();
     const seats = useSelector(getSeats);
     const requests = useSelector(getRequests);
+
+    const [socket, setSocket] = useState(null);
 
     useEffect(() => {
 
         const socketConnectionUrl = getSocketConnectionUrl();
         const newSocket = io(socketConnectionUrl);
+
         setSocket(newSocket);
 
 
         newSocket.on('connect', () => {
             console.log('New socket connected:', newSocket.id);
 
+
             newSocket.on('seatsUpdated', handleSeatsUpdated);
         });
+
 
         const handleSeatsUpdated = (updatedSeats) => {
             console.log('Updated seats:', updatedSeats);
@@ -44,32 +48,23 @@ const SeatChooser = ({ chosenDay, chosenSeat, updateSeat }) => {
             newSocket.disconnect();
             newSocket.off('seatsUpdated', handleSeatsUpdated);
         };
-    }, [dispatch]);
+    }, [dispatch, chosenDay]);
 
-    const handleReserve = (seat) => {
-        const reservationData = {
-            day: chosenDay,
-            seat: seat,
-            client: 'Your Client Name',
-            email: 'your@email.com',
-        };
-
-        socket.emit('reserveSeat', reservationData);
-    };
-
-    useEffect(() => {
-        dispatch(loadSeatsRequest());
-    }, [dispatch, chosenSeat]);
 
     const isTaken = (seatId) => {
         return (seats.some(item => (item.seat === seatId && item.day === chosenDay)));
     }
+
 
     const prepareSeat = (seatId) => {
         if(seatId === chosenSeat) return <Button key={seatId} className="seats__seat" color="primary">{seatId}</Button>;
         else if(isTaken(seatId)) return <Button key={seatId} className="seats__seat" disabled color="secondary">{seatId}</Button>;
         else return <Button key={seatId} color="primary" className="seats__seat" outline onClick={(e) => updateSeat(e, seatId)}>{seatId}</Button>;
     }
+
+
+    const takenSeats = seats.filter(seat => seat.day === chosenDay).length;
+    const freeSeats = 50 - takenSeats;
 
     return (
         <div>
@@ -79,6 +74,7 @@ const SeatChooser = ({ chosenDay, chosenSeat, updateSeat }) => {
             { (requests['LOAD_SEATS'] && requests['LOAD_SEATS'].success) && <div className="seats">{[...Array(50)].map((x, i) => prepareSeat(i+1) )}</div>}
             { (requests['LOAD_SEATS'] && requests['LOAD_SEATS'].pending) && <Progress animated color="primary" value={50} /> }
             { (requests['LOAD_SEATS'] && requests['LOAD_SEATS'].error) && <Alert color="warning">Couldn't load seats...</Alert> }
+            <p>Free seats: {freeSeats}/50</p>
         </div>
     )
 }
