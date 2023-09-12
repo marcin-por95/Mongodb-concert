c2f4944af3ccd391fdad6b466a2546a26473a818
Marcin Poręba <marcin.poreba1995@gmail.com>
2023-09-13 00:40:28 +0200

    replit fix problem white window #4

--- server.js
+++ server.js
@@ -1,62 +1,55 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const socket = require('socket.io');
+const helmet = require('helmet');
 const mongoose = require('mongoose');
-const dbUrl = 'mongodb://localhost:27017/NewWaveDB';
+
 const app = express();
+app.use(helmet());
 
-const testimonialsRouter = require('./routes/testimonials.routes');
-const concertsRouter = require('./routes/concerts.routes');
-const seatsRouter = require('./routes/seats.routes');
+// import routes
+const testimonialsRoutes = require('./routes/testimonials.routes');
+const concertsRoutes = require('./routes/concerts.routes');
+const seatsRoutes = require('./routes/seats.routes');
 
-const corsOptions = {
-    origin: 'http://localhost:3000',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-};
+const server = app.listen(process.env.PORT || 8000, () => {
+    console.log('Server is running...');
+});
 
-app.use(cors(corsOptions));
+
+const io = socket(server);
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
-
-app.use('/api', testimonialsRouter);
-app.use('/api', concertsRouter);
-app.use('/api', seatsRouter);
+app.use((req, res, next) => {
+    req.io = io;
+    next();
+})
+app.use('/api', testimonialsRoutes);
+app.use('/api', concertsRoutes);
+app.use('/api', seatsRoutes);
 app.use(express.static(path.join(__dirname, '/client/build')));
+
 app.use((req, res) => {
-    res.status(404).json({message: 'Not found...'});
-});
-
-app.use((err, req, res) => {
-    console.log(err);
-    console.log('test');
-    res.status(err.status || 500).json();
-});
-
-mongoose.set("debug", (collectionName, method, query, doc) => {
-    console.log(`ERROR ${collectionName}.${method}`, JSON.stringify(query), doc);
+    res.status(404).json({ message: 'Not found...' });
 });
 
 mongoose.connect('mongodb+srv://marcinporeba1995:yjf10D2eJ9m6pwef@cluster0.jsugvbv.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true,  useUnifiedTopology: true  });
 const db = mongoose.connection;
-
 db.once('open', () => {
-    console.log('Connected to the database');
+    console.log('Connected to database')
+})
+
+db.on('error', err => console.log('Error' + err))
+
+io.on('connection', (socket) =>{
+    console.log('User connected');
 });
-db.on('error', err => console.log('Error ' + err));
-
-
-/* ENDPOINTS  */
-
-
-app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/client/build/index.html'));
 });
 
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/client/build/index.html'));
-});
+module.exports = server;
