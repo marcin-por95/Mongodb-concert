715fb4975579c73ad346e63f67681ce717b0abd9
Marcin Poręba <marcin.poreba1995@gmail.com>
2023-09-12 17:47:44 +0200

    Fix db connection issues

--- models/concert.model.js
+++ models/concert.model.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
 const concertSchema = new mongoose.Schema({
+    _id: { type: mongoose.Types.ObjectId, required: true },
+    id: { type: String, required: true },
     performer: {type: String, required: true},
     genre: {type: String, required: true},
     price: {type: Number, required: true},
--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const socket = require('socket.io');
 const mongoose = require('mongoose');
-
+const dbUrl = 'mongodb://localhost:27017/NewWaveDB';
 const app = express();
 
 const testimonialsRouter = require('./routes/testimonials.routes');
@@ -17,39 +16,37 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-
-const server = app.listen(process.env.PORT || 8000, () => {
-    console.log('Server is running...');
-});
-const io = socket(server);
-
-app.use((req, res, next) => {
-    req.io = io;
-    next();
-});
-
-io.on('connection', (socket) => {
-    console.log('New socket connected:', socket.id);
-});
-
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 
 
-const dbUrl = 'mongodb://localhost:27017/NewWaveDB';
-mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log('Connected to MongoDB');
-    })
-    .catch((err) => {
-        console.error('Error connecting to MongoDB:', err);
-    });
+app.use('/api', testimonialsRouter);
+app.use('/api', concertsRouter);
+app.use('/api', seatsRouter);
+
+app.use((req, res) => {
+    res.status(404).json({message: 'Not found...'});
+});
+
+app.use((err, req, res) => {
+    console.log(err);
+    console.log('test');
+    res.status(err.status || 500).json();
+});
+
+mongoose.set("debug", (collectionName, method, query, doc) => {
+    console.log(`ERROR ${collectionName}.${method}`, JSON.stringify(query), doc);
+});
+mongoose.connect(dbUrl, {useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection;
+db.once('open', () => {
+    console.log('Connected to the database');
+});
+db.on('error', err => console.log('Error ' + err));
+
 
 /* ENDPOINTS  */
 
-app.use('/api/testimonials', testimonialsRouter);
-app.use('/api/concerts', concertsRouter);
-app.use('/api/seats', seatsRouter);
 
 app.use(express.static(path.join(__dirname, '/client/build')));
 
@@ -57,6 +54,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/client/build/index.html'));
 });
 
-app.use((req, res) => {
-    res.status(404).json({ message: 'Not found...' });
+
+const server = app.listen(process.env.PORT || 8000, () => {
+    console.log('Server is running...');
 });
