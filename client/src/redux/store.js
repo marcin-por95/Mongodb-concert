d9585758e12aa76b6bada777e5dcd9c7eac79a17
Marcin Poręba <marcin.poreba1995@gmail.com>
2023-09-12 23:04:56 +0200

    replit fix problem white window

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,20 +1,20 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
+// import reducers
 import concerts from './concertsRedux';
 import seats from './seatsRedux';
 
+// combine reducers
 const rootReducer = combineReducers({
 	concerts,
 	seats,
 });
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
 	rootReducer,
-	compose(
-		applyMiddleware(thunk),
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-	)
+	composeEnhancers(applyMiddleware(thunk))
 );
 
 export default store;
